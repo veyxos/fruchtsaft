bb4d01d24e6daed289febed662762e019ea47e0d
veyxos <veyxos@gmail.com>
2016-12-06 18:59:58 +0100

    bugfixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,7 @@ ready(function(){
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       quotes = JSON.parse(xmlhttp.responseText);
       quote_array = window.location.pathname == "/" || window.location.pathname == "/index.html" ? quotes["fruchtsaft"] : quotes["anni"];
-      for (var i = 0; i < quote_array.length; i++) {
+      for (var i = 0; i <= quote_array.length; i++) {
         codes.push(quote_array[i]["c"]);
         codes[quote_array[i]["c"]] = i;
       }
@@ -47,7 +47,7 @@ ready(function(){
         document.getElementById("cite").innerHTML = "– " + quotes["by"][0][quote_array[quote_no]["by"]] + ", " + quote_array[quote_no]["y"];
       });
       document.getElementById("link__tweet").href = "https://twitter.com/intent/tweet?text=" + encodeURI(quote_array[quote_no]["q"].replace(/<br>/g,"\n").replace(/<strong>/g,"").replace(/<\/strong>/g,"")) + "&hashtags=fruchtsaft,quote&url=" + encodeURI("https://fruchtsaft.veyxos.de/?c=" + quote_no) + "&via=TheEmerl&related=TheEmerl%3ADer%20Typ%20um%20den%27s%20hier%20geht%2CVeyxos%3AEr%20hat%20das%20alles%20hier%20verbrochen!";
-      document.getElementById("link__copy").setAttribute("data-clipboard-text","http://fruchtsaft.veyxos.de/?c=" + quote_no);
+      document.getElementById("link__copy").setAttribute("data-clipboard-text","http://fruchtsaft.veyxos.de/?q=" + quote_no);
 
       document.getElementById("quote").innerHTML = quote_array[quote_no]["q"];
       document.getElementById("cite").innerHTML = "– " + quotes["by"][0][quote_array[quote_no]["by"]] + ", " + quote_array[quote_no]["y"];
